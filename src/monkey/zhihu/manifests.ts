c19ccb4a8c569e1443c91a4ea92504cfd23b4f40
kisstar <dwh.chn@foxmail.com>
2022-02-12 20:30:43 +0800

    feat(zhihu): 更新路由匹配规则

--- src/monkey/zhihu/manifests.ts
+++ src/monkey/zhihu/manifests.ts
@@ -4,8 +4,8 @@
 // @version      0.0.1
 // @description  支持登录模态框隐藏。
 // @author       Kisstar
-// @match        https://www.zhihu.com/question/*
-// @match        https://zhuanlan.zhihu.com/p/*
+// @match        https://www.zhihu.com/*
+// @match        https://zhuanlan.zhihu.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // @license      MIT License
