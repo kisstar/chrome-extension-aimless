2e5ebf56d4462a06313f4582231b78bb34179fd4
kisstar <dwh.chn@foxmail.com>
2022-02-12 17:11:19 +0800

    build: 修复单脚本打包失败

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -12,14 +12,16 @@ const { resolve, generateEntry } = require('./shared');
 const argv = argvParser(process.argv.slice(2));
 const name = argv.name || argv._[0];
 
+const srcDir = resolve('src', monkeyDir);
+const inputs = generateEntry(monkeyDir, srcDir, 'rollup');
+
 // 当前仅对单脚本进行打包
 if (!name) {
   console.error('请指定需要打包的脚本名称。');
   process.exit(1);
 }
 
-const srcDir = resolve('src', monkeyDir);
-const input = generateEntry(monkeyDir, srcDir, 'rollup');
+const input = inputs.filter(item => item.includes(`src/monkey/${name}/index.ts`));
 const metaHeader = fs.readFileSync(resolve(dirname(input[0]), 'manifests.ts'));
 
 export default {
